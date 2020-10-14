30bb01a2c12b6d8a8a1dee9cd91a82598c65dced
Rafal Swietek <raf.swietek@gmail.com>
2020-10-14 22:37:40 +0200

    Initial configuration

---
+++ index.js
@@ -0,0 +1,37 @@
+module.exports = {
+  extends: [
+    'airbnb-base',
+  ],
+  parser: 'babel-eslint',
+  rules: {
+    'comma-dangle': [
+      'error',
+      {
+        arrays: 'always-multiline',
+        objects: 'always-multiline',
+        imports: 'always-multiline',
+        exports: 'always-multiline',
+        functions: 'always-multiline',
+      },
+    ],
+    'no-underscore-dangle': [
+      'error',
+      {
+        allow: [
+          '_id',
+        ],
+        allowAfterThis: true,
+        allowAfterSuper: true,
+      },
+    ],
+    'no-confusing-arrow': 'error',
+    'class-methods-use-this': 0,
+    complexity: ['error', 5],
+    'max-nested-callbacks': ['error', 3],
+    'max-depth': ['error', 3],
+    'max-classes-per-file': ['error', 1],
+    'no-else-return': 'error',
+    'max-params': ['error', 5],
+    'function-paren-newline': ['error', 'consistent'],
+  },
+};
